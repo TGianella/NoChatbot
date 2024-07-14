829ecfecd5355c9d2e7079642547e5b7e97f8565
Théo Gianella <theo.gianella@proton.me>
2024-07-14 19:39:34 +0200

    feat(fetch): first working version of rendering fetched form json

--- src/app/RequestForm/RequestForm.tsx
+++ src/app/RequestForm/RequestForm.tsx
@@ -12,40 +12,90 @@ import { JsonForms } from "@jsonforms/react";
 import Button from "@mui/material/Button";
 import { ErrorObject } from "ajv";
 
+const url = "https://mistralgagnant.alwaysdata.net/api";
+const initialResponseData = {
+  schema: {},
+  uischema: {},
+  data: {},
+};
+
 export const RequestForm = () => {
-  const [data, setData] = useState(InitialData);
+  const [formData, setFormData] = useState(InitialData);
   const [errors, setErrors] = useState<ErrorObject[]>([]);
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const [debugFormSent, setDebugFormSent] = useState(false);
+  const [responseData, setResponseData] = useState(initialResponseData);
 
   const currentValidationMode = formSubmitted
     ? "ValidateAndShow"
     : "ValidateAndHide";
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+    if (errors.length === 0) {
+      setDebugFormSent(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        setResponseData(json);
+      } catch (error) {
+        console.error(error.message);
+      }
+    } else {
+      setDebugFormSent(false);
+    }
+  };
+
   return (
     <div className="App">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          setFormSubmitted(true);
-          if (errors.length === 0) {
-            console.log("coucou", data);
-          }
-        }}
-      >
+      {responseData.schema ? (
         <JsonForms
-          schema={Schema}
-          uischema={UiSchema}
-          data={data}
+          schema={responseData.schema}
+          uischema={responseData.uischema}
+          data={responseData.data}
           renderers={materialRenderers}
           cells={materialCells}
           onChange={({ data, errors }) => {
-            setData(data);
+            setFormData(data);
             setErrors(errors ? (errors as ErrorObject[]) : []);
           }}
           validationMode={currentValidationMode}
         />
-        <Button type="submit">Submit</Button>
-      </form>
+      ) : (
+        <>
+          <form onSubmit={handleSubmit}>
+            <JsonForms
+              schema={Schema}
+              uischema={UiSchema}
+              data={formData}
+              renderers={materialRenderers}
+              cells={materialCells}
+              onChange={({ data, errors }) => {
+                setFormData(data);
+                setErrors(errors ? (errors as ErrorObject[]) : []);
+              }}
+              validationMode={currentValidationMode}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+          {process.env.NODE_ENV === "development" ? (
+            debugFormSent ? (
+              <p className="p-1 mt-1 bg-green-400 rounded border border-green-700">
+                Form data sent
+              </p>
+            ) : (
+              <p className="p-1 mt-1 bg-red-400 rounded border border-red-700">
+                Form data not sent
+              </p>
+            )
+          ) : null}
+        </>
+      )}
     </div>
   );
 };
