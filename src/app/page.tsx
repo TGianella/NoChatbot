122c0bb3081bbe31d445375b26b6fda1c2236324
Théo Gianella <theo.gianella@proton.me>
2024-07-18 10:58:19 +0200

    feat(mui): add material theme to fix ui issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { RequestPanel } from "@/app/components/RequestPanel/RequestPanel";
 import { ResponsePanel } from "@/app/components/ResponsePanel/ResponsePanel";
 import { FinalAnswerPanel } from "@/app/components/FinalAnswerPanel/FinalAnswerPanel";
 import { useState } from "react";
+import { ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
 export type ResponseData = {
   schema: object;
@@ -14,6 +16,33 @@ export type ResponseData = {
   uid: string;
 };
 
+const theme = createTheme({
+  components: {
+    MuiFormControl: {
+      styleOverrides: {
+        root: {
+          marginTop: "10px",
+        },
+      },
+    },
+    MuiSlider: {
+      defaultProps: {
+        marks: true,
+        valueLabelDisplay: "on",
+      },
+      styleOverrides: {
+        valueLabel: {
+          fontSize: 12,
+          fontWeight: "normal",
+          top: -6,
+          backgroundColor: "unset",
+          color: "blue",
+        },
+      },
+    },
+  },
+});
+
 export default function Home() {
   //@ts-expect-error
   const [responseData, setResponseData] = useState<ResponseData>({});
@@ -23,24 +52,26 @@ export default function Home() {
   const [answerFormSubmitted, setAnswerFormSubmitted] = useState(false);
 
   return (
-    <main className="flex p-5">
-      <RequestPanel
-        setResponseData={setResponseData}
-        setRequestSent={setRequestSent}
-        requestSent={requestSent}
-        formSubmitted={requestFormSubmitted}
-        setFormSubmitted={setRequestFormSubmitted}
-      />
-      <ResponsePanel
-        responseData={responseData}
-        setFinalAnswer={setFinalAnswer}
-        isLoading={requestFormSubmitted}
-        setFormSubmitted={setAnswerFormSubmitted}
-      />
-      <FinalAnswerPanel
-        finalAnswer={finalAnswer}
-        isLoading={answerFormSubmitted}
-      />
-    </main>
+    <ThemeProvider theme={theme}>
+      <main className="flex m-5 border border-gray-50">
+        <RequestPanel
+          setResponseData={setResponseData}
+          setRequestSent={setRequestSent}
+          requestSent={requestSent}
+          formSubmitted={requestFormSubmitted}
+          setFormSubmitted={setRequestFormSubmitted}
+        />
+        <ResponsePanel
+          responseData={responseData}
+          setFinalAnswer={setFinalAnswer}
+          isLoading={requestFormSubmitted}
+          setFormSubmitted={setAnswerFormSubmitted}
+        />
+        <FinalAnswerPanel
+          finalAnswer={finalAnswer}
+          isLoading={answerFormSubmitted}
+        />
+      </main>
+    </ThemeProvider>
   );
 }
