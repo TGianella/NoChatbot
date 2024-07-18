60552ed1970932580be36e2573456143649579af
Théo Gianella <theo.gianella@proton.me>
2024-07-18 13:03:35 +0200

    chore(alert): use mui alert

--- src/app/components/DataSentAlert/DataSentAlert.tsx
+++ src/app/components/DataSentAlert/DataSentAlert.tsx
@@ -1,15 +1,16 @@
+import { Alert } from "@mui/material";
+
 export const DataSentAlert = ({
   debugFormSent,
 }: {
   debugFormSent: boolean;
 }) => {
-  return debugFormSent ? (
-    <p className="p-1 mt-1 bg-green-400 rounded border border-green-700">
-      Form data sent
-    </p>
-  ) : (
-    <p className="p-1 mt-1 bg-red-400 rounded border border-red-700">
-      Form data not sent
-    </p>
+  const severity = debugFormSent
+    ? ("success" as "success")
+    : ("error" as "error");
+  return (
+    <Alert severity={severity}>
+      Form data{debugFormSent ? "" : " not"} sent
+    </Alert>
   );
 };
